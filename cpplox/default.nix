d9ad8fa6504a60baf3ae582f55dbb78abfdbedd1
Jamie Quigley <jamie@quigley.xyz>
2021-11-07 00:59:02 +0000

    Add nix build scripts for cpplox and clox_pp

---
+++ cpplox/default.nix
@@ -0,0 +1,14 @@
+{ pkgs ? import <nixpkgs> { }
+  #, cpkgs ? import (fetchTarball "https://github.com/sciencentistguy/nixpkgs/archive/e601d30ad4fe5e65bd49f52414b92dbef47aa87f.tar.gz") { inherit pkgs; }
+}:
+with pkgs; stdenv.mkDerivation {
+  name = "cpplox";
+
+  src = ./.;
+
+  nativeBuildInputs = [
+    cmake
+    ninja
+  ];
+
+}
